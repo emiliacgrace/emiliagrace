ed5eb853fd7a4f332bb49a6330173943346c2f23
Emilia Grace <emiliacgrace@gmail.com>
2025-06-21 14:28:04 +0100

    Change Admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,4 @@
-from django.contrib.admin import AdminSite
+from django.contrib import admin
 from .models import BlogPost
 
-
-class CustomAdminSite(AdminSite):
-    site_header = "Admin 💖"
-    site_title = "Admin"
-    index_title = "Ready to write a new blog post?"
-
-    def each_context(self, request):
-        context = super().each_context(request)
-        context["css_files"] = ["css/admin_custom.css"]
-        return context
-
-
-admin_site = CustomAdminSite(name='custom_admin')
-
-admin_site.register(BlogPost)
+admin.site.register(BlogPost)
--- emiliagrace/urls.py
+++ emiliagrace/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from blog import views
-from blog.admin import admin_site
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('post/<slug:slug>/', views.blog_detail, name='blog_detail'),
-    path("admin/", admin_site.urls),
+    path("admin/", admin.site.urls),
 ]
